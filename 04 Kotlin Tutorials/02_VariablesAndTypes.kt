61439e6d2ddb422788ceb62789fc96dce9969a42
bamakant <bamakantkar@gmail.com>
2019-01-17 22:55:13 +0530

    Add 02_VariablesAndTypes.kt

---
+++ 04 Kotlin Tutorials/02_VariablesAndTypes.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+
+    // To print strings
+    var name: String = "Bama Kant"
+    println(name)
+
+// Declare Int variable and print it.
+    var num: Int = 23
+    println(num)
+
+// Declare double for storing floating point numbers
+var decimal: Double = 6.5
+println(decimal)
+
+//Devision operation
+var firstNum: Int = 64
+var secondNum: Int = 5
+var resultDiv: Int = firstNum/secondNum
+println(resultDiv)
+println("The result is $resultDiv")
+
+//String concatenation
+val firstName: String = "Hello"
+val secondName: String = "Kotlin!"
+var concatString: String = firstName +" " +secondName
+println(concatString)
+
+
+
+} 
