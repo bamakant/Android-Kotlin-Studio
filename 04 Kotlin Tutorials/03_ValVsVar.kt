1c9f8febe5576e8755dea6f5c92847d857716bea
bamakant <bamakantkar@gmail.com>
2019-01-17 23:08:56 +0530

    Add 03_ValVsVar.kt

---
+++ 04 Kotlin Tutorials/03_ValVsVar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+
+    //var stands for variable
+    var name = "Bama Kant"
+
+    //val stands for value which means it cannot be changed again and again.
+    val roll = 123456
+
+    println("name and roll is $roll")
+}
