9f43f5a14ed4a7832c4d66d6fd894beba66ce6cb
bamakant <bamakantkar@gmail.com>
2019-01-24 12:42:23 +0530

    Add 06_loop.kt

---
+++ 04 Kotlin Tutorials/06_Loop.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+//     // in range loop
+//     for(i in 1..10){
+//         println("$i squared is ${i * i}")
+//     }    
+
+//     // unitll rane loop
+//     for (i in 0 until 10){
+//             println("$i squared is ${i * i}")
+//     }
+//     // downTo loop
+//  for (i in 10 downTo 0){
+//             println("$i squared is ${i * i}")
+//     }   
+ // downtTo step loop
+ for (i in 10 downTo 0 step 2){
+            println("$i squared is ${i * i}")
+    }  
+
+}
