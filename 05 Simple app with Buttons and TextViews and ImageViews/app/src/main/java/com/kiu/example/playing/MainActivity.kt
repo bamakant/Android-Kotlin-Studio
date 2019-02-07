c4f65cfa7319621d56ba23af81b9747fb0394f1f
bamakant <bamakantkar@gmail.com>
2019-02-07 12:12:17 +0530

    Change folder name.

---
+++ 05 Simple app with Buttons and TextViews and ImageViews/app/src/main/java/com/kiu/example/playing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kiu.example.playing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.note_layout)
+    }
+}
